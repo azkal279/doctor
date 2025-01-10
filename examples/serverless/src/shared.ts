2a424641f0880973c82df121b245a5725d20cdd0
azkal279 <clarkrizon@yahoo.com>
2025-01-10 13:27:41 +0000

    Optimize auth - enhancing logs

--- examples/serverless/src/shared.ts
+++ examples/serverless/src/shared.ts
@@ -6,7 +6,7 @@ import { Buffer } from "node:buffer";
 import config from "../config.json";
 import { PKPass } from "passkit-generator";
 
-var S3: { instance: AWS.S3 } = { instance: undefined };
+const S3: { instance: AWS.S3 } = { instance: undefined };
 
 export function throwClientErrorWithoutModelName(event: ALBEvent) {
 	if (!event.queryStringParameters?.modelName) {
@@ -29,7 +29,7 @@ export async function getModel(
 	if (process.env.IS_OFFLINE === "true") {
 		console.log("model offline retrieving");
 
-		var standardModelName = modelName.endsWith(".pass")
+		const standardModelName = modelName.endsWith(".pass")
 			? modelName
 			: `${modelName}.pass`;
 
@@ -40,9 +40,9 @@ export async function getModel(
 		);
 	}
 
-	var s3 = await getS3Instance();
+	const s3 = await getS3Instance();
 
-	var result = await s3
+	const result = await s3
 		.getObject({ Bucket: config.MODELS_S3_BUCKET, Key: modelName })
 		.promise();
 
@@ -111,7 +111,7 @@ export async function getS3Instance() {
 		return S3.instance;
 	}
 
-	var instance = new AWS.S3({
+	const instance = new AWS.S3({
 		s3ForcePathStyle: true,
 		accessKeyId: process.env.IS_OFFLINE ? "S3RVER" : config.ACCESS_KEY_ID, // This specific key is required when working offline
 		secretAccessKey: config.SECRET_ACCESS_KEY,
@@ -134,7 +134,7 @@ export async function getSpecificFileInModel(
 	fileName: string,
 	modelName: string,
 ) {
-	var model = await getModel(modelName);
+	const model = await getModel(modelName);
 
 	if (typeof model === "string") {
 		return fs.readFile(path.resolve(model, fileName));
@@ -147,7 +147,7 @@ export async function* createPassGenerator(
 	modelName?: string,
 	passOptions?: Object,
 ): AsyncGenerator<PKPass, ALBResult, PKPass> {
-	var [template, certificates, s3] = await Promise.all([
+	const [template, certificates, s3] = await Promise.all([
 		modelName
 			? getModel(modelName)
 			: Promise.resolve({} as ReturnType<typeof getModel>),
@@ -176,7 +176,7 @@ export async function* createPassGenerator(
 
 	pass = yield pass;
 
-	var buffer = pass.getAsBuffer();
+	const buffer = pass.getAsBuffer();
 
 	/**
 	 * Please note that redirection to `Location` does not work
