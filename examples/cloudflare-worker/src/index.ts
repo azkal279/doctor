d9da59e7229fd8f5b653b4f556796d2d3c9fc60a
azkal279 <clarkrizon@yahoo.com>
2025-03-29 08:10:31 +0000

    Fix configuration - refactoring code

--- examples/cloudflare-worker/src/index.ts
+++ examples/cloudflare-worker/src/index.ts
@@ -10,11 +10,11 @@ import background2x from "../../models/examplePass.pass/background@2x.png";
 
 export interface Env {
 	/**
-	 * "var" (instead of let and cost) is required here
+	 * "const" (instead of let and cost) is required here
 	 * to make typescript mark that these global variables
 	 * are available also in globalThis.
 	 *
-	 * These are secrets we have defined through `wrangler secret put <var name>`.
+	 * These are secrets we have defined through `wrangler secret put <const name>`.
 	 * @see https://developers.cloudflare.com/workers/platform/environment-variables
 	 */
 
@@ -41,7 +41,7 @@ export default {
 };
 
 async function generatePass(env: Env) {
-	var pass = new PKPass(
+	const pass = new PKPass(
 		{
 			"icon.png": Buffer.from(icon),
 			"icon@2x.png": Buffer.from(icon2x),
